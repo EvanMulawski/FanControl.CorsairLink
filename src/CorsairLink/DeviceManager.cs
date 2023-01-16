622b3bece938cfc48949fea89cec7925515fddfe
Evan Mulawski <EvanMulawski@users.noreply.github.com>
2023-01-15 19:36:19 -0500

    add device manager logging

--- src/CorsairLink/DeviceManager.cs
+++ src/CorsairLink/DeviceManager.cs
@@ -1,4 +1,5 @@
 ﻿using HidSharp;
+using System.Text;
 
 namespace CorsairLink;
 
@@ -6,33 +7,57 @@ public static class DeviceManager
 {
     public static SupportedDeviceCollection GetSupportedDevices(ILogger? logger)
     {
-        var hidDevices = DeviceList.Local.GetHidDevices(vendorID: HardwareIds.CorsairVendorId);
-        var supportedDevices = hidDevices
+        var corsairDevices = DeviceList.Local
+            .GetHidDevices(vendorID: HardwareIds.CorsairVendorId)
+            .ToList();
+        logger?.LogDevices(corsairDevices, "Corsair device(s)");
+        
+        var supportedDevices = corsairDevices
             .Where(x => HardwareIds.SupportedProductIds.Contains(x.ProductID) && x.GetMaxOutputReportLength() > 0)
+            .ToList();
+        logger?.LogDevices(supportedDevices, "supported Corsair device(s)");
+
+        var supportedDevicesByProductId = supportedDevices
             .ToLookup(x => x.ProductID);
 
         var collection = new SupportedDeviceCollection();
 
         collection.CommanderProDevices
-            .AddRange(supportedDevices[HardwareIds.CorsairCommanderProProductId]
+            .AddRange(supportedDevicesByProductId[HardwareIds.CorsairCommanderProProductId]
             .Select(x => new CommanderProDevice(new HidSharpDeviceProxy(x), logger)));
 
         collection.CommanderProDevices
-            .AddRange(supportedDevices[HardwareIds.CorsairObsidian1000DCommanderProProductId]
+            .AddRange(supportedDevicesByProductId[HardwareIds.CorsairObsidian1000DCommanderProProductId]
             .Select(x => new CommanderProDevice(new HidSharpDeviceProxy(x), logger)));
 
         collection.CommanderCoreDevices
-            .AddRange(supportedDevices[HardwareIds.CorsairCommanderCoreXTProductId]
+            .AddRange(supportedDevicesByProductId[HardwareIds.CorsairCommanderCoreXTProductId]
             .Select(x => new CommanderCoreDevice(new HidSharpDeviceProxy(x), new CommanderCoreDeviceOptions { IsFirstChannelExt = false }, logger)));
 
         collection.CommanderCoreDevices
-            .AddRange(supportedDevices[HardwareIds.CorsairCommanderCoreProductId]
+            .AddRange(supportedDevicesByProductId[HardwareIds.CorsairCommanderCoreProductId]
             .Select(x => new CommanderCoreDevice(new HidSharpDeviceProxy(x), new CommanderCoreDeviceOptions { IsFirstChannelExt = true }, logger)));
 
         collection.CommanderCoreDevices
-            .AddRange(supportedDevices[HardwareIds.CorsairCommanderSTProductId]
+            .AddRange(supportedDevicesByProductId[HardwareIds.CorsairCommanderSTProductId]
             .Select(x => new CommanderCoreDevice(new HidSharpDeviceProxy(x), new CommanderCoreDeviceOptions { IsFirstChannelExt = true }, logger)));
 
         return collection;
+    }
+
+    private static void LogDevices(this ILogger? logger, IReadOnlyCollection<HidDevice> devices, string description)
+    {
+        if (logger is null)
+        {
+            return;
+        }
+
+        var sb = new StringBuilder();
+        sb.AppendLine($"DeviceManager: Found {devices.Count} {description}");
+        foreach (var device in devices)
+        {
+            sb.AppendLine($"  name={device.GetProductName()}, devicePath={device.DevicePath}");
+        }
+        logger.Log(sb.ToString());
     }
 }
